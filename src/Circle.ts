8c774a0591e84fb42c7cda84fbc9c7412ff637cb
Wouter van Heeswijk <wouter.vanheeswijk@tomtom.com>
2016-12-23 15:33:34 +0100

    Added x, y parameters to circle

--- src/Circle.ts
+++ src/Circle.ts
@@ -2,6 +2,8 @@ import Node, {Bounds, Point} from './Node';
 import NodeFixedBounds from './NodeFixedBounds';
 
 export interface CircleParameters {
+    x?: number;
+    y?: number;
     radius: number;
     lineWidth?: number;
     strokeStyle?: string;
@@ -9,19 +11,23 @@ export interface CircleParameters {
 }
 
 class Circle extends NodeFixedBounds {
+    private x: number;
+    private y: number;
     private radius: number;
     private lineWidth: number;
     private strokeStyle: string;
     private fillStyle: string;
 
-    constructor({radius, lineWidth = 1, strokeStyle = 'rgba(0, 0, 0, 1)', fillStyle = 'rgba(255, 255, 255, 1)'}: CircleParameters) {
-        const minX = -radius - lineWidth / 2;
-        const minY = -radius - lineWidth / 2;
-        const maxX = radius + lineWidth / 2;
-        const maxY = radius + lineWidth / 2;
+    constructor({x = 0, y = 0, radius, lineWidth = 1, strokeStyle = 'rgba(0, 0, 0, 1)', fillStyle = 'rgba(255, 255, 255, 1)'}: CircleParameters) {
+        const minX = x - radius - lineWidth / 2;
+        const minY = y - radius - lineWidth / 2;
+        const maxX = x + radius + lineWidth / 2;
+        const maxY = y + radius + lineWidth / 2;
         const bounds: Bounds = {minX, minY, maxX, maxY};
 
         super(bounds);
+        this.x = x;
+        this.y = y;
         this.radius = radius;
         this.lineWidth = lineWidth;
         this.strokeStyle = strokeStyle;
@@ -29,12 +35,12 @@ class Circle extends NodeFixedBounds {
     }
 
     draw(context: CanvasRenderingContext2D): void {
-        const {radius, strokeStyle, lineWidth, fillStyle} = this;
+        const {x, y, radius, strokeStyle, lineWidth, fillStyle} = this;
         const oldStrokeStyle = context.strokeStyle;
         const oldFillStyle = context.fillStyle;
         const oldLineWidth = context.lineWidth;
         context.beginPath();
-        context.arc(0, 0, radius, 0, 2 * Math.PI, false);
+        context.arc(x, y, radius, 0, 2 * Math.PI, false);
         context.strokeStyle = strokeStyle;
         context.fillStyle = fillStyle;
         context.lineWidth = lineWidth;
@@ -49,8 +55,8 @@ class Circle extends NodeFixedBounds {
     }
 
     intersection({x, y}: Point): Node {
-        const {radius, lineWidth} = this;
-        if (x ** 2 + y ** 2 <= (radius + lineWidth / 2) ** 2) {
+        const {x: ox, y: oy, radius, lineWidth} = this;
+        if ((x - ox) ** 2 + (y - oy) ** 2 <= (radius + lineWidth / 2) ** 2) {
             return this;
         }
     }
