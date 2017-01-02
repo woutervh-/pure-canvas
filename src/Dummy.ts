407fd742b3fdb2c1c9319455e373723202ed7116
Wouter van Heeswijk <wouter.vanheeswijk@tomtom.com>
2017-01-02 14:30:26 +0100

    Added dummy node that renders nothing

---
+++ src/Dummy.ts
@@ -0,0 +1,17 @@
+import Node, {Point} from './Node';
+import NodeFixedBounds from './NodeFixedBounds';
+
+class Dummy extends NodeFixedBounds {
+    constructor() {
+        super({minX: 0, minY: 0, maxX: 0, maxY: 0});
+    }
+
+    draw(context: CanvasRenderingContext2D): void {
+    }
+
+    intersection({x, y}: Point): Node {
+        return null;
+    }
+}
+
+export default Dummy;
