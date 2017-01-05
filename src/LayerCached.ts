aeb1e2ad440a8772178b027929baea9ec6ea3598
Wouter van Heeswijk <wouter.vanheeswijk@tomtom.com>
2017-01-05 17:24:26 +0100

    Prevent attempt to drav 0x0 cache layers (FF bug)

--- src/LayerCached.ts
+++ src/LayerCached.ts
@@ -44,28 +44,32 @@ export default class LayerCached extends Layer {
 
     drawDeferred(context: CanvasRenderingContext2D, stepAccumulator: Array<() => void>, commitAccumulator: Array<() => void>): void {
         const {minX, minY, maxX, maxY} = this.getBounds();
+        const width = maxX - minX;
+        const height = maxY - minY;
 
-        if (!this.cache) {
-            this.cache = document.createElement('canvas');
-            this.cache.width = maxX - minX;
-            this.cache.height = maxY - minY;
-            const cacheContext = this.cache.getContext('2d');
-            const cacheStepAccumulator: Array<() => void> = [];
-            const cacheCommitAccumulator: Array<() => void> = [];
-            super.drawDeferred(cacheContext, cacheStepAccumulator, cacheCommitAccumulator);
+        if (width > 0 && height > 0) {
+            if (!this.cache) {
+                this.cache = document.createElement('canvas');
+                this.cache.width = width;
+                this.cache.height = height;
+                const cacheContext = this.cache.getContext('2d');
+                const cacheStepAccumulator: Array<() => void> = [];
+                const cacheCommitAccumulator: Array<() => void> = [];
+                super.drawDeferred(cacheContext, cacheStepAccumulator, cacheCommitAccumulator);
 
-            stepAccumulator.push(() => cacheContext.translate(-minX, -minY));
-            for (const cacheStep of cacheStepAccumulator) {
-                stepAccumulator.push(cacheStep);
+                stepAccumulator.push(() => cacheContext.translate(-minX, -minY));
+                for (const cacheStep of cacheStepAccumulator) {
+                    stepAccumulator.push(cacheStep);
+                }
+                for (const cacheCommit of cacheCommitAccumulator) {
+                    stepAccumulator.push(cacheCommit);
+                }
+                stepAccumulator.push(() => cacheContext.translate(minX, minY));
             }
-            for (const cacheCommit of cacheCommitAccumulator) {
-                stepAccumulator.push(cacheCommit);
-            }
-            stepAccumulator.push(() => cacheContext.translate(minX, minY));
+
+            commitAccumulator.push(() => {
+                context.drawImage(this.cache, minX, minY, width, height);
+            });
         }
-
-        commitAccumulator.push(() => {
-            context.drawImage(this.cache, minX, minY, maxX - minX, maxY - minY);
-        });
     }
 };
