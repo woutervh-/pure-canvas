5df549f1b9a082e9ddb401e8e1cb0b84ecd06abc
Wouter van Heeswijk <wouter.vanheeswijk@tomtom.com>
2017-04-14 14:57:13 +0200

    Target ES6, and add guard in TreeManager against infinite bounding boxes

--- src/TreeManager.ts
+++ src/TreeManager.ts
@@ -17,18 +17,20 @@ export default class TreeManager {
         this.tree.clear();
         this.nodeIndexable.index((node: Node, zIndex: number, transformers: Array<Transformer>) => {
             const bounds = node.getBounds();
-            const {x: x1, y: y1} = transformers.reduce((point: Point, transformer: Transformer) => transformer.transform(point), {x: bounds.minX, y: bounds.minY});
-            const {x: x2, y: y2} = transformers.reduce((point: Point, transformer: Transformer) => transformer.transform(point), {x: bounds.maxX, y: bounds.maxY});
-            const {x: x3, y: y3} = transformers.reduce((point: Point, transformer: Transformer) => transformer.transform(point), {x: bounds.minX, y: bounds.maxY});
-            const {x: x4, y: y4} = transformers.reduce((point: Point, transformer: Transformer) => transformer.transform(point), {x: bounds.maxX, y: bounds.minY});
-            const minX = Math.min(x1, x2, x3, x4);
-            const minY = Math.min(y1, y2, y3, y4);
-            const maxX = Math.max(x1, x2, x3, x4);
-            const maxY = Math.max(y1, y2, y3, y4);
-            this.tree.insert({
-                minX, minY, maxX, maxY,
-                transformers, node, zIndex
-            });
+            if (Number.isFinite(bounds.minX) && Number.isFinite(bounds.minY) && Number.isFinite(bounds.maxX) && Number.isFinite(bounds.maxY)) {
+                const {x: x1, y: y1} = transformers.reduce((point: Point, transformer: Transformer) => transformer.transform(point), {x: bounds.minX, y: bounds.minY});
+                const {x: x2, y: y2} = transformers.reduce((point: Point, transformer: Transformer) => transformer.transform(point), {x: bounds.maxX, y: bounds.maxY});
+                const {x: x3, y: y3} = transformers.reduce((point: Point, transformer: Transformer) => transformer.transform(point), {x: bounds.minX, y: bounds.maxY});
+                const {x: x4, y: y4} = transformers.reduce((point: Point, transformer: Transformer) => transformer.transform(point), {x: bounds.maxX, y: bounds.minY});
+                const minX = Math.min(x1, x2, x3, x4);
+                const minY = Math.min(y1, y2, y3, y4);
+                const maxX = Math.max(x1, x2, x3, x4);
+                const maxY = Math.max(y1, y2, y3, y4);
+                this.tree.insert({
+                    minX, minY, maxX, maxY,
+                    transformers, node, zIndex
+                });
+            }
         }, 0);
     }
 
